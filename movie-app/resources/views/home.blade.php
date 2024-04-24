659dc0b1dffac2740be342ce7279e61a3cba0dd5
sarahelshark <sarahelsh183@gmail.com>
2024-04-24 18:06:24 +0200

    add
    layout

--- movie-app/resources/views/home.blade.php
+++ movie-app/resources/views/home.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        
-    </head>
-    <body class="antialiased">
-
-   
+@extends('layouts.app')
+    @section('content') 
         @foreach($movies as $movie)
 
         <div class="card">
@@ -23,7 +7,5 @@
         </div>
 
         @endforeach
+    @endsection
         
-        
-    </body>
-</html>
